dc1b7f7da247aad7d2ce2860994efb97a52fb18f
bunnyisariiider <tmd7764@gmail.com>
2025-08-12 15:34:49 +0900

    [refactor] UserService 레이어분리-비즈니스로직개선

---
+++ src/javaproject/domain/User.java
@@ -0,0 +1,25 @@
+package javaproject.domain;
+
+public class User {
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String id, String password, String name, String email){
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+
+    //유효성 검증 메서드 추가 (버그 수정)
+    public boolean isValid(){
+        return id != null& !id.isEmpty() && password != null && password.length() >= 6;
+
+    }
+}
+
